8f6fa43c996cfcde90cf469daeac7946687131f7
OriChaiMatan <ori.chaimatan@gmail.com>
2024-07-29 19:59:58 +0300

    LocalStorage service and useMemo setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import "bootstrap/dist/css/bootstrap.min.css"
+import { useMemo } from "react"
 import { Container } from "react-bootstrap"
 import { Routes, Route, Navigate } from "react-router-dom"
+import { v4 as uuidV4 } from "uuid"
+
+import { useLocalStorage } from "./services/useLocalStorage"
 
 import { NewNote } from "./cmps/NewNote"
 
 export type Note = {
   id: string
 } & NoteData
+
+export type RawNote = {
+  id: string
+} & RawNoteData
+
+export type RawNoteData = {
+  title: string
+  markdown: string
+  tagIds: string[]
+}
 
 export type NoteData = {
   title: string
@@ -20,7 +34,23 @@ export type Tag = {
 }
 
 function App() {
-  const [notes, setNotes] = useLocalStorage<RawNote[]>("notes", [])
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", [])
+  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", [])
+
+  const notesWithTags = useMemo(() => {
+    return notes.map(note => {
+      return { ...note, tags: tags.filter(tag => note.tagIds.includes(tag.id)) }
+    })
+  }, [notes, tags])
+
+  function onCreateNote({ tags, ...data }: NoteData) {
+    setNotes(prevNotes => {
+      return [
+        ...prevNotes,
+        { ...data, id: uuidV4(), tagIds: tags.map(tag => tag.id) },
+      ]
+    })
+  }
 
   return (
     <Container>
