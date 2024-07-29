2549e5c479701298d2ffd00495005625df3b3aff
OriChaiMatan <ori.chaimatan@gmail.com>
2024-07-29 19:27:01 +0300

    New Note design

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import { Container } from "react-bootstrap"
 import { Routes, Route, Navigate } from "react-router-dom"
 
+import { NewNote } from "./cmps/NewNote"
+
 function App() {
   return (
     <Container>
       <Routes>
         <Route path="/" element={<h1>home</h1>} />
-        <Route path="/new" element={<h1>new</h1>} />
+        <Route path="/new" element={<NewNote/>} />
         <Route path="/:id" element={<h1>show</h1>} />
         <Route path="/:id/edit" element={<h1>edit</h1>} />
         <Route path="*" element={<Navigate to="/" />} />
